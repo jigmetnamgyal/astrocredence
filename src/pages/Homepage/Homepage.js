9600df676f0e933a651e9181ec6ca8e36d5b0285
JigmeT12 <jigmetashi02@gmail.com>
2021-05-13 00:01:32 +0600

    Added: intro to homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -3,6 +3,11 @@ import styles from './homepage.module.css';
 const Homepage = () => {
     return (
         <div className={styles.homepageContainer}>
+            <div className={styles.homeTitle}>
+                <h1>Astro Credence</h1>
+               <i><p>For the faintest stars still shine</p></i>
+            </div>
+           
         </div>
     )
 }
