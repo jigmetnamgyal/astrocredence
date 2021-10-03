5c7b8255ca395ba9c6875d0b38023344e7d8b023
ayushhagarwal <ayushgrwl365@gmail.com>
2021-10-03 16:14:07 +0530

    footer done but not a 100 percent

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import {useState} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Homepage from './pages/Homepage/Homepage';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Blog from './pages/Blog/Blog';
-import ContactUs from './pages/ContactUs/ContactUs';
-import Newsletter from './pages/Newsletter/Newsletter';
-import NavBar from './components/nav/NavigationBar';
-import Mainpage from './pages/mainpage/Mainpage';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Blog from "./pages/Blog/Blog";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import Newsletter from "./pages/Newsletter/Newsletter";
+import NavBar from "./components/nav/NavigationBar";
+import Mainpage from "./pages/mainpage/Mainpage";
+import Footer from "./components/Footer/Footer";
 
-
-import './App.css'
+import "./App.css";
 function App() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    }
-    return (
-        <Router>
-            <div className="App">
-              <NavBar />
-                <Switch>
-                    <Route exact path="/" component={Mainpage}/>
-                    <Route path="/aboutUs" component={AboutUs}/>
-                    <Route path="/blog" component={Blog}/>
-                    <Route path="/contactUs" component={ContactUs}/>
-                    <Route path="/newsletter" component={Newsletter}/>
-                </Switch>
-            </div>
-        </Router>
-    );
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Mainpage} />
+          <Route path="/aboutUs" component={AboutUs} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/contactUs" component={ContactUs} />
+          <Route path="/newsletter" component={Newsletter} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,66 +1,102 @@
-import styles from './footer.module.css';
-import {AiOutlineInstagram,AiOutlineGithub, AiFillFacebook, AiOutlineTwitter, AiFillLinkedin} from 'react-icons/ai'
-import {Link} from 'react-router-dom';
-import me from '../../assets/logo.svg';
+import styles from "./footer.module.css";
+import {
+  AiOutlineInstagram,
+  AiOutlineGithub,
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
+import me from "../../assets/logo.svg";
 
 const Footer = () => {
-    return (
-        <div className={styles.footer}>
-            <div className={styles.footerChildOneContainer}>
-                <div className={styles.footerOne}>
-                    <div className={styles.titleHolder}>
-                        <img className={styles.imgLogo} src={me} alt="me pic"/>
-                        <h5>Astro Credence</h5>
-                    </div>
-
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    <div className={styles.footerOneMediaContainer}>
-                        <a
-                            href="https://www.instagram.com/jigmetashinamgyal/"
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            className={styles.footerInsta}><AiOutlineInstagram/>
-                        </a>
-                        <a
-                            href="https://www.facebook.com/Mugiwara.D.Jigme619/"
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            className={styles.footerFacebook}><AiFillFacebook/>
-                        </a>
-                        <a href="https://twitter.com/JigmeTashiNamg1" rel="noopener noreferrer" target="_blank" className={styles.footerTwitter}><AiOutlineTwitter/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/jigme-tashi-namgyal-511a811a3/" rel="noopener noreferrer" target="_blank" className={styles.footerLinkedin}><AiFillLinkedin/>
-                        </a>
-                        <a href="https://github.com/JigmeT123" rel="noopener noreferrer" target="_blank" className={styles.footerLinkedin}><AiOutlineGithub/>
-                        </a>
-                    </div>
-                </div>
-
-                <div className={styles.footerTwo}>
-                    <h4>Quick Links</h4>
-                    <Link to="/">Home</Link>
-                    <Link to="/bookShelf">BookShelf</Link>
-                    <Link to="/books">Books Review</Link>
-                    <Link to="/blog">Blog</Link>
-                    <Link to="/projects">Projects</Link>
-                </div>
-                <div className={styles.footerFour}>
-                    <h4>About Us</h4>
-                    <Link to="/aboutMe">About Me</Link>
-                </div>
-            </div>
-            <div className={styles.line}></div>
-            <div className={styles.footerChildTwoContainer}>
-                <div className={styles.footerTwoContainer}>
-                    <p>
-                        &copy;Astro Credence 2021-
-                        <span>
-                           all rights reserved</span>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footerChildOneContainer}>
+        <div className={styles.footerOne}>
+          <div className={styles.titleHolder}>
+            <img className={styles.imgLogo} src={me} alt="me pic" />
+            <h5>Astro Credence</h5>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
-    )
-}
 
-export default Footer
+        <div className={styles.footerTwo}>
+          <h4>Quick Links</h4>
+          <Link to="/">Home</Link>
+          <Link to="/bookShelf">BookShelf</Link>
+          <Link to="/books">Books Review</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/projects">Projects</Link>
+        </div>
+        <div className={styles.footerFour}>
+          <h4>About Us</h4>
+          <Link to="/aboutMe">About Me</Link>
+        </div>
+      </div>
+      <div className={styles.footerOneMediaContainer}>
+        <a
+          href="https://www.instagram.com/jigmetashinamgyal/"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={styles.footerInsta}
+        >
+          <AiOutlineInstagram />
+        </a>
+        <a
+          href="https://www.facebook.com/Mugiwara.D.Jigme619/"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={styles.footerFacebook}
+        >
+          <AiFillFacebook />
+        </a>
+        <a
+          href="https://twitter.com/JigmeTashiNamg1"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={styles.footerTwitter}
+        >
+          <AiOutlineTwitter />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jigme-tashi-namgyal-511a811a3/"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={styles.footerLinkedin}
+        >
+          <AiFillLinkedin />
+        </a>
+        <a
+          href="https://github.com/JigmeT123"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={styles.footerLinkedin}
+        >
+          <AiOutlineGithub />
+        </a>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.footerChildTwoContainer}>
+        <div className={styles.footerTwoContainer}>
+          <p>
+            &copy;Astro Credence 2021-
+            <span>all rights reserved</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
