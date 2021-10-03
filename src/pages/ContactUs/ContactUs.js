b0d97f8609e457273b20315de6f06bd074598c95
DADDY-DOUBLESHOT <prathams1001@gmail.com>
2021-10-03 17:00:27 +0530

    Contact us updated with background and new styles

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -35,66 +35,68 @@ const ContactUs = () => {
     }
 
     return (
-        <div className={styles.contactUsContainer}>
-            <div className={styles.contactUsTitleHolder}>
-                <div className={styles.contactz}>
-                    <p>Got a Questions? Contact Us</p>
-                   
-                    <p className={styles.titleContent}>We’re here to help and answer any question 
-                    you might have. We look forward to hearing from you</p>
+        <div className="styles.contactUsBackground">
+            <div className={styles.contactUsContainer}>
+                <div className={styles.contactUsTitleHolder}>
+                    <div className={styles.contactz}>
+                        <p>Got a Questions? Contact Us</p>
+                    
+                        <p className={styles.titleContent}>We’re here to help and answer any question 
+                        you might have. We look forward to hearing from you</p>
+                    </div>
                 </div>
+
+                <form className={styles.formContainer}>
+                    <div className={styles.formStyle}>
+                        <label for="exampleInputEmail1">First Name</label>
+                        <input
+                            onChange={firstNameHandle}
+                            value={firstName}
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"/>
+
+                    </div>
+
+                    <div className={styles.formStyle}>
+                        <label for="exampleInputEmail1">Last Name</label>
+                        <input
+                            onChange={lastNameHandle}
+                            value={lastName}
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"/>
+
+                    </div>
+
+                    <div className={styles.formStyle}>
+                        <label for="exampleInputEmail1">Email</label>
+                        <input
+                            onChange={emailHandle}
+                            value={email}
+                            type="email"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"/>
+
+                    </div>
+
+                    <div className={styles.formStyle}>
+                        <label for="exampleInputEmail1">Message</label>
+                        <textarea
+                            onChange={messageHandle}
+                            value={message}
+                            type="text"
+                            class="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"/>
+
+                    </div>
+                    <Button className={styles.contactUsButton} onClick={handleClick}>Send Message</Button>
+                </form>
             </div>
-
-            <form className={styles.formContainer}>
-                <div className={styles.formStyle}>
-                    <label for="exampleInputEmail1">First Name</label>
-                    <input
-                        onChange={firstNameHandle}
-                        value={firstName}
-                        type="text"
-                        class="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"/>
-
-                </div>
-
-                <div className={styles.formStyle}>
-                    <label for="exampleInputEmail1">Last Name</label>
-                    <input
-                        onChange={lastNameHandle}
-                        value={lastName}
-                        type="text"
-                        class="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"/>
-
-                </div>
-
-                <div className={styles.formStyle}>
-                    <label for="exampleInputEmail1">Email</label>
-                    <input
-                        onChange={emailHandle}
-                        value={email}
-                        type="email"
-                        class="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"/>
-
-                </div>
-
-                <div className={styles.formStyle}>
-                    <label for="exampleInputEmail1">Message</label>
-                    <textarea
-                        onChange={messageHandle}
-                        value={message}
-                        type="text"
-                        class="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"/>
-
-                </div>
-                <Button className={styles.contactUsButton} onClick={handleClick}>Send Message</Button>
-            </form>
         </div>
     )
 }
