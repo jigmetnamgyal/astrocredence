94f42a77c651da278efb85f2f38bf65d45ada723
Mitushi-23 <mitushi2agrawal@gmail.com>
2021-10-03 22:02:34 +0530

    modified navbar

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,18 +1,18 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo1.svg';
 import styles from './navbar.module.css';
 import Ham from './tools/hamBurger';
 
-const Navbar = ({hamHandler}) => {
+const Navbar = ({ hamHandler }) => {
 
     const [handleShow, setHandleShow] = useState(false);
-    useEffect(()=>{
-        window.addEventListener('scroll', ()=> {
-            if(window.scrollY > 350){
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 350) {
                 setHandleShow(true)
-            }else{
+            } else {
                 setHandleShow(false)
             }
         })
@@ -21,30 +21,32 @@ const Navbar = ({hamHandler}) => {
         };
     }, [])
 
+    
+
     return (
         <header className={`${styles.navBar} ${handleShow && styles.header__scrolled}`}>
-                <div className={styles.hamHolder}>
-                    <Ham click = {hamHandler}/>
-                </div>
-               <div className={styles.navbarLogo}>
-               <img className={styles.astroCLogo} src={logo} alt="Astro Credence Logo"/>
-                </div>
-                   
-                <nav className={styles.header__navigation}>
-                    <ul>
-                        <Link to="/"> Home</Link>
-                        <Link to="/aboutUs">About Us</Link>
-                        <Link to="/contactUs">Contact Us</Link>
-                        <Link to="/projects">Blog</Link>
-                        <Link to="/newsletter">Newsletter</Link>
-                        <Link to="/shop">Shop</Link>
-                    </ul>
-                </nav>
-                <div className={styles.socialMediaContainer}>
-                    <a className={styles.solmedia} href="https://www.instagram.com/astrocredence/" target="_blank" rel="noreferrer noopener"><AiOutlineInstagram /> </a>
-                    <a className={styles.solmedia} href="/" rel="noreferrer noopener"><AiOutlineFacebook /> </a>
-                    <a className={styles.solmedia} href="/" rel="noreferrer noopener"><AiOutlineTwitter /> </a>
-                </div>
+            <div className={styles.hamHolder}>
+                <Ham click={hamHandler} />
+            </div>
+            <div className={styles.navbarLogo}>
+                <img className={styles.astroCLogo} src={logo} alt="Astro Credence Logo" />
+            </div>
+
+            <nav className={styles.header__navigation}>
+                <ul>
+                    <Link to="/" style={{ borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2 }}> Home</Link>
+                    <Link to="/aboutUs" className={styles.style}>About Us</Link>
+                    <Link to="/contactUs" className={styles.style}>Contact Us</Link>
+                    <Link to="/projects"  className={styles.style}>Blog</Link>
+                    <Link to="/newsletter"  className={styles.style}>Newsletter</Link>
+                    <Link to="/shop"  className={styles.style}>Shop</Link>
+                </ul>
+            </nav>
+            <div className={styles.socialMediaContainer}>
+                <a className={styles.solmedia} href="https://www.instagram.com/astrocredence/" target="_blank" rel="noreferrer noopener"><AiOutlineInstagram /> </a>
+                <a className={styles.solmedia} href="/" rel="noreferrer noopener"><AiOutlineFacebook /> </a>
+                <a className={styles.solmedia} href="/" rel="noreferrer noopener"><AiOutlineTwitter /> </a>
+            </div>
         </header>
     )
 }
