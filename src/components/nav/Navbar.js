99b80e34c8c273c20d856d01f31aadc66c2a760d
Mitushi-23 <mitushi2agrawal@gmail.com>
2021-10-04 16:28:11 +0530

    changed css of active links

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import {  NavLink } from 'react-router-dom';
 import logo from '../../assets/logo1.svg';
 import styles from './navbar.module.css';
 import Ham from './tools/hamBurger';
 
-const Navbar = ({ hamHandler }) => {
+const Navbar = ({hamHandler}) => {
 
     const [handleShow, setHandleShow] = useState(false);
     useEffect(() => {
@@ -21,8 +21,6 @@ const Navbar = ({ hamHandler }) => {
         };
     }, [])
 
-    
-
     return (
         <header className={`${styles.navBar} ${handleShow && styles.header__scrolled}`}>
             <div className={styles.hamHolder}>
@@ -34,12 +32,29 @@ const Navbar = ({ hamHandler }) => {
 
             <nav className={styles.header__navigation}>
                 <ul>
-                    <Link to="/" style={{ borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2 }}> Home</Link>
-                    <Link to="/aboutUs" className={styles.style}>About Us</Link>
-                    <Link to="/contactUs" className={styles.style}>Contact Us</Link>
-                    <Link to="/projects"  className={styles.style}>Blog</Link>
-                    <Link to="/newsletter"  className={styles.style}>Newsletter</Link>
-                    <Link to="/shop"  className={styles.style}>Shop</Link>
+                    <NavLink activeStyle={{
+                        borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2
+                    }} to="/" className={styles.style}> Home</NavLink>
+                    
+                    <NavLink activeStyle={{
+                        borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2
+                    }} to="/aboutUs" className={styles.style}  >About Us</NavLink>
+                    
+                    <NavLink activeStyle={{
+                        borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2
+                    }} to="/contactUs" className={styles.style}>Contact Us</NavLink>
+                    
+                    <NavLink activeStyle={{
+                        borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2
+                    }} to="/projects" className={styles.style}>Blog</NavLink>
+                   
+                    <NavLink activeStyle={{
+                        borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2
+                    }} to="/newsletter" className={styles.style}>Newsletter</NavLink>
+                    
+                    <NavLink activeStyle={{
+                        borderBottomStyle: 'solid', borderBottomColor: "blue", borderBottomWidth: 2
+                    }} to="/shop" className={styles.style}>Shop</NavLink>
                 </ul>
             </nav>
             <div className={styles.socialMediaContainer}>
