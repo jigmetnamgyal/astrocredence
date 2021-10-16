f71a0b821b0c0ee2d00e036ff00f8d5899a045fd
Nitin Nair <nitin.dreamern89@gmail.com>
2021-10-16 17:13:30 +0530

    Fixes #2. Added AboutUS page UI.

---
+++ src/pages/AboutUs/team.js
@@ -0,0 +1,52 @@
+import Girl1 from "../../assets/AboutUs/girl1.svg"
+import Girl2 from "../../assets/AboutUs/girl2.svg"
+import Girl3 from "../../assets/AboutUs/girl3.svg"
+import Girl4 from "../../assets/AboutUs/girl4.svg"
+import Boy1 from "../../assets/AboutUs/boy1.svg"
+import Boy2 from "../../assets/AboutUs/boy2.svg"
+import Boy3 from "../../assets/AboutUs/boy3.svg"
+import Boy4 from "../../assets/AboutUs/boy4.svg"
+
+const members = [
+    {
+        avatar: Girl1,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Boy1,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Girl2,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Boy2,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Girl3,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Boy3,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Girl4,
+        name: "Name",
+        role: "Role"
+    },
+    {
+        avatar: Boy4,
+        name: "Name",
+        role: "Role"
+    },
+];
+export default members;
